025be481fb1efff200004ca848673ed893dc548b
Chad MILLER <chad@cornsilk.net>
2019-09-01 11:38:55 -0700

    Add some javadoc comments for memoizers

--- memoizer.kt
+++ memoizer.kt
@@ -20,6 +20,7 @@ import java.io.File
 import java.util.Base64
 
 class MemoizeSuspFun<K, V> (val subject_function: suspend (K) -> V) {
+    /** Memoize a suspending function that takes a single hashable parameter.  */
 
     private var visibility_horizon_time = 0L
 
@@ -30,18 +31,25 @@ class MemoizeSuspFun<K, V> (val subject_function: suspend (K) -> V) {
     private val storage = java.util.Collections.synchronizedMap(HashMap<K, AnnotatedValue<V>>())
 
     fun expire_older_than_ms(time: Long) {
+        /** Calls against the memoized function that have not been used since
+         * given point in time (expressed in milliseconds since epoch) are no
+         * longer returned. 
+         */
         this.visibility_horizon_time = time
     }
 
     fun expire_all() {
+        /** Expunge all cached return values. */
         storage.clear()
     }
 
     fun expire_one(name: K) {
+        /** Expunge the cached return value for the call of a single value. */
         storage.remove(name)
     }
 
     suspend fun run(arg: K): V {
+        /** Run the memoized function with a given parameter. */
         val item = storage.get(arg)
         if ((item != null) && (item.atime > this.visibility_horizon_time)) {
             item.atime = System.currentTimeMillis()
@@ -57,6 +65,7 @@ class MemoizeSuspFun<K, V> (val subject_function: suspend (K) -> V) {
 
 /* Memoizer for mundane functions. */
 class MemoizeFun<K, V> (val subject_function: (K) -> V) {
+    /** Memoize a regular function that takes a single hashable parameter.  */
 
     private var visibility_horizon_time = 0L
 
@@ -67,18 +76,25 @@ class MemoizeFun<K, V> (val subject_function: (K) -> V) {
     private val storage = java.util.Collections.synchronizedMap(HashMap<K, AnnotatedValue<V>>())
 
     fun expire_older_than_ms(time: Long) {
+        /** Calls against the memoized function that have not been used since
+         * given point in time (expressed in milliseconds since epoch) are no
+         * longer returned. 
+         */
         this.visibility_horizon_time = time
     }
 
     fun expire_all() {
+        /** Expunge all cached return values. */
         storage.clear()
     }
 
     fun expire_one(name: K) {
+        /** Expunge the cached return value for the call of a single value. */
         storage.remove(name)
     }
 
     fun run(arg: K): V {
+        /** Run the memoized function with a given parameter. */
         val item = storage.get(arg)
         if ((item != null) && (item.atime > this.visibility_horizon_time)) {
             item.atime = System.currentTimeMillis()
